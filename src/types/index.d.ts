198c013cb15ddc28bbe1eeba448905a951616793
avik <aviksain007@gmail.com>
2024-09-01 21:04:22 +0530

    fixed the webhook

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,14 +5,14 @@ declare type CreateUserParams = {
   clerkId: string;
   email: string;
   username: string;
-  firstName: string | null;
-  lastName: string | null;
+  firstname: string | null;
+  lastname: string | null;
   photo: string;
 };
 
 declare type UpdateUserParams = {
-  firstName: string | null;
-  lastName: string | null;
+  firstname: string | null;
+  lastname: string | null;
   username: string;
   photo: string;
 };
