750274ed8da7e4755471a842253478fff4ee7d92
avik <aviksain007@gmail.com>
2024-10-28 21:35:59 +0530

    testing

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -11,6 +11,8 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET!;
 
+  console.log("clark webhook invoked");
+  
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
